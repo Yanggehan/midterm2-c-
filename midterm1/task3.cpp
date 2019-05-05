bbc80a334a4557da53fc2793b7bb3dfb9eec4182
Yanggehan <33634849+Yanggehan@users.noreply.github.com>
2019-05-04 17:23:01 -0700

    resubmit

--- midterm1/task3.cpp
+++ midterm1/task3.cpp
@@ -1,46 +1,76 @@
-#include <iostream>
-#include <stdlib.h>
-
-#include <time.h>
+#include<iostream>
+#include<time.h>
+#include<stdlib.h>
 using namespace std;
-class numberA {
-private:
-	float *numberArr;
-	int number;
-	int hight;
-	int  low;
-public:
-	numberA(double A,int n , int x);
-	numberA();
-	double highest(int n)const { int hight = n; };
-	double lowest(int n )const { int low = n; };
-	int gethight() { return n; };
-	int getlow() { return x; };
-};
-numberA::numberA(double A, int n, int x)
+class floatarray
 {
-	n = hight;
-	x = low;
-}
-
-
+private:
+	float *array;
+	int length =0;
+public:
+	floatarray(int p)
+	{
+		array = new float[p];
+		length = p;
+	}
+	~floatarray()
+	{
+		delete[] array;
+	}
+	void store(int k, float val)
+	{
+		
+			array[k] = val;
+	}
+	float getvalue(int k)
+	{
+	
+			return array[k];
+	}
+	float getmax()
+	{
+		float max = array[0];
+		for (int i = 1; i < length; i++)
+		{
+			if (array[i] > max)
+				max = array[i];
+		}
+		return max;
+	}
+	float getmin()
+	{
+		float min = array[0];
+		for (int i = 1; i < length; i++)
+		{
+			if (array[i] < min)
+				min = array[i];
+		}
+		return min;
+	}
+	float get_average()
+	{
+		float sum = 0;
+		for (int i = 0; i < length; i++)
+			sum = sum + array[i];
+		return sum / length;
+	}
+	void printArr() {
+		for (int i = 0; i < length; i++) {
+			cout << array[i] << ",";
+		}
+	}
+};
 int main()
 {
-	int n;
-	int n2;
-	int n3;
-	
-	cout << "size of your array" << endl;
-	cin >> n ;
-	new double A[n];
-	cout << "the number " << endl;
-	cout << "the number big" << endl;
-	cin >> n2;
-	cout << "the number small" << endl;
-	cin >> n3;
-	numberA(A , n2,n3)
-	
-	for (int i = 0; i < n; i++) {
-
-	}
+	floatarray my_array(10);
+	for (int i = 0; i < 10; i++)
+		my_array.store(i, rand() % 100);
+	cout << "number is " << endl;
+	my_array.printArr();
+	cout << " " << endl;
+	cout << "max value in array is " << my_array.getmax() << endl;
+	cout << "min value in array is " << my_array.getmin() << endl;
+	cout << "average value of elements in array is " << my_array.get_average() << endl;
+	system("pause");
+	return 0;
 }
